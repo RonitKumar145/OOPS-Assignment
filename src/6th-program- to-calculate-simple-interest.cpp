c7b8d3bb125723b801913f994847506444123f4f
ronit kumar <ronitkumar145@gmail.com>
2025-12-07 01:38:48 +0530

    6th program to find simple interest

---
+++ src/6th-program- to-calculate-simple-interest.cpp
@@ -0,0 +1,28 @@
+//Ronit Kumar
+//roll no -48, REG NO -10056
+//BTECH-CSE, 2ND YEAR
+
+#include<iostream> 
+
+using namespace std; 
+int main(){
+  float principleAMT, interestRate, timePeriod, simpleInterest;                   // declaring variable
+
+  cout<<"enter principle amount: ";
+  cin>>principleAMT;
+
+  cout<<"enter rate of interest: ";
+  cin>>interestRate;
+
+  cout<<"enter time in years: ";
+  cin>>timePeriod;
+
+  simpleInterest=(principleAMT*interestRate*timePeriod)/100;             // simple interest => (p*r*t)/100
+
+  cout<<"simple interest: "<<endl;
+
+  return 0;
+
+
+
+}
