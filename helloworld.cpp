0df1e72acb4edeeeb538b66345223ad4d0cae678
ronit kumar <ronitkumar145@gmail.com>
2025-12-06 23:21:53 +0530

    ADDING NAME AND DETAILS

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,10 @@
-#include<iostream>
-using namespace std;
-int main(){
-  cout<<"Hello World";
-  return 0;
+//Ronit Kumar
+//roll no -48, REG NO -10056
+//BTECH-CSE, 2ND YEAR
+
+#include<iostream>                    //header file
+using namespace std;                  //namespace declaration
+int main(){                           //execution begins
+  cout<<"Hello World";                //statement (cout = this prints program)
+  return 0;                           //return
 }
