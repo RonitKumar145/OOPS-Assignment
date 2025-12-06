3e44df4fe93d4951ca5f2e5f146d1e9bea932e0c
ronit kumar <ronitkumar145@gmail.com>
2025-12-07 00:06:06 +0530

    2nd program data type and sizes.

---
+++ Program-to-demonstrate-data-types-and-sizes.cpp
@@ -0,0 +1,38 @@
+//Ronit Kumar
+//roll no -48, REG NO -10056
+//BTECH-CSE, 2ND YEAR
+
+#include<iostream> 
+
+using namespace std; 
+int main(){
+  int age=21;
+  cout<<"int value = "<<age<<endl;                           // integer data type
+  cout<<"int size = "<<sizeof(int)<<" Bytes"<<endl;
+ 
+
+  char grade='A';
+  cout<<"char value = "<<grade<<endl;                           // character data type
+  cout<<"char size = "<<sizeof(char)<<" Bytes"<<endl;
+
+  float salary=20000.11;
+  cout<<"float value = "<<salary<<endl;                           // floating point data type to 
+  cout<<"float size = "<<sizeof(float)<<" Bytes"<<endl;
+
+  double balance =150.116;
+  cout<<"double value = "<<balance<<endl;                           // double data type
+  cout<<"double size = "<<sizeof(double)<<" Bytes"<<endl;
+
+  bool student= true;
+   cout<<"boolean value = "<<student<<endl;                           // boolean data type( true=1, false=0)
+  cout<<"boolean size = "<<sizeof(bool)<<" Bytes"<<endl;
+
+  string name =" Ronit";
+   cout<<"string value = "<<name<<endl;                           // string type data
+  cout<<"string size = "<<sizeof(string)<<" Bytes"<<endl;
+
+  return 0;
+
+
+
+  }
