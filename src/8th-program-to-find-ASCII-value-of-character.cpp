9473cea508460eb0a200991c0c0a52b29ba7444c
ronit kumar <ronitkumar145@gmail.com>
2025-12-07 22:00:56 +0530

    8th program to find the ASCII value of a charcter

---
+++ src/8th-program-to-find-ASCII-value-of-character.cpp
@@ -0,0 +1,17 @@
+//Ronit Kumar
+//roll no -48, REG NO -10056
+//BTECH-CSE, 2ND YEAR
+
+#include<iostream> 
+
+using namespace std; 
+int main(){
+  char character;                                    // Character value declaration
+
+  cout<<"enter character to find ASCII value: ";       
+  cin>>character;                                     
+
+  cout<<"ASCII value: "<<character<<"="<<int(character)<<endl;                  // to display character ASCII value using type casting
+
+  return 0;
+}
